007e5b14fe80ecacaf887af93f50fa6a521363fa
Krishna Bajaj <bajaj.krishna01062002@gmail.com>
2022-08-05 18:35:45 +0530

    Added Camera Support in Home Screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,11 @@
+
+
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,38 +14,109 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
-    final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
       appBar: AppBar(title: const Text(_title)),
-      body: Padding(
-        padding: const EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Signed In as',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              user.email!,
-              style: const TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(50),
-              ),
-              icon: const Icon(Icons.arrow_back, size: 32),
-              label: const Text(
-                'Sign Out',
-                style: TextStyle(fontSize: 24),
-              ),
-              onPressed: () => FirebaseAuth.instance.signOut(),
-            ),
-          ],
-        )
-      ),
+      body: const MyStatefulWidget(),
     );
   }
 }
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+
+  File? image;
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if(image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch(e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future pickImageC() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+      if(image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch(e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context){
+    final user = FirebaseAuth.instance.currentUser!;
+    return Scaffold(
+      //appBar: AppBar(title: const Text(_title)),
+      body: Padding(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MaterialButton(
+                  color: Colors.blue,
+                  child: const Text(
+                      "Pick Image from Gallery",
+                      style: TextStyle(
+                          color: Colors.white70, fontWeight: FontWeight.bold
+                      )
+                  ),
+                  onPressed: () {
+                    pickImage();
+                  }
+              ),
+              MaterialButton(
+                  color: Colors.blue,
+                  child: const Text(
+                      "Pick Image from Camera",
+                      style: TextStyle(
+                          color: Colors.white70, fontWeight: FontWeight.bold
+                      )
+                  ),
+                  onPressed: () {
+                    pickImageC();
+                  }
+              ),
+              const SizedBox(height: 40),
+              image != null? Image.file(image!,height: 250,width: 250,): const Text('No Image Selected'),
+              const Text(
+                'Signed In as',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                user.email!,
+                style: const TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                ),
+                icon: const Icon(Icons.arrow_back, size: 32),
+                label: const Text(
+                  'Sign Out',
+                  style: TextStyle(fontSize: 24),
+                ),
+                onPressed: () => FirebaseAuth.instance.signOut(),
+              ),
+            ],
+          )
+      ),
+    );
+  }
+
+}
+
+
+
