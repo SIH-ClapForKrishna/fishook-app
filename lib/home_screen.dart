24eaabb0535c7450db96c7d10aa1a457cecead61
Kohav Dey <deykohav@gmail.com>
2022-08-16 18:37:17 +0530

    ML #2, not working

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,6 +7,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 
+import 'package:tflite/tflite.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -30,7 +32,14 @@ class MyStatefulWidget extends StatefulWidget {
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
-  File? image;
+  late double _imageWidth;
+  late double _imageHeight;
+  bool _busy = false;
+
+  late List _recognitions;
+
+
+  late File image;
   Future pickImage() async {
     try {
       final image = await ImagePicker().pickImage(source: ImageSource.gallery);
@@ -48,21 +57,147 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       if(image == null) return;
       final imageTemp = File(image.path);
       setState(() => this.image = imageTemp);
+      predictImage(image);
     } on PlatformException catch(e) {
       print('Failed to pick image: $e');
     }
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _busy = true;
+    loadModel().then((val){
+      setState(() {
+        _busy = false;
+      });
+    });
+  }
+
+  loadModel() async {
+    Tflite.close();
+    try {
+      String? res = await Tflite.loadModel(
+          model: "assets/mobilenet_v1_1.0_224.tflite",
+          labels: "assets/mobilenet_v1_1.0_224.txt",
+          numThreads: 1,
+          // defaults to 1
+          isAsset: true,
+          // defaults to true, set to false to load resources outside assets
+          useGpuDelegate: false // defaults to false, set to true to use GPU delegate
+      );
+      print (res);
+    }on PlatformException{
+      print ("Failed to load the model");
+    }
+  }
+
+  @override
+  void dispose() async {
+    // TODO: implement dispose
+    super.dispose();
+    await Tflite.close();
+  }
+
+  predictImage (image) async{
+    await mobilenet(image);
+
+    FileImage(image)
+        .resolve(const ImageConfiguration())
+        .addListener((ImageStreamListener((ImageInfo info, bool _) {
+      setState (() {
+        _imageWidth = info.image.width.toDouble();
+        _imageHeight = info.image.height.toDouble();
+      });
+    })));
+
+    setState((){
+      image = image;
+      _busy = false;
+    });
+  }
+
+  mobilenet (File image) async {
+    var recognitions = await Tflite.detectObjectOnImage(
+      path: image.path,
+      model: "Fish Detection",
+      threshold: 0.3,
+      imageMean: 0.0,
+      imageStd: 255.0,
+      numResultsPerClass: 1
+    );
+
+    setState(() {
+      _recognitions = _recognitions;
+    });
+  }
+
+  List<Widget> renderBoxes (Size screen) {
+    if (_recognitions == null) return [];
+    if (_imageWidth == null || _imageHeight == null) return [];
+
+    double factorX = screen.width;
+    double factorY = _imageHeight/_imageHeight*screen.width;
+
+    Color blue = Colors.blue;
+
+    return _recognitions.map((re){
+      return Positioned(
+      left: re["rect"]["x"]*factorX,
+      top: re["rect"]["y"]*factorY,
+      width: re["rect"]["w"]*factorX,
+      height: re["rect"]["h"]*factorY,
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border.all(
+          color: blue,
+          width: 3,
+        )),
+        child: Text(
+          "${re["detectedClass"]} ${(re["confidenceInClass"]*100).toStringAsFixed(0)}",
+        style: TextStyle(
+          background: Paint()..color=blue,
+          color: Colors.white,
+          fontSize: 15,
+        ),
+      ),
+      ) ,
+      );
+    }).toList();
+  }
+
+  @override
   Widget build(BuildContext context){
+    
+    Size size = MediaQuery.of(context).size;
+    List<Widget> stackChildren = [];
+
+    stackChildren.add(Positioned(
+      top: 0.0,
+      left: 0.0,
+      width: size.width,
+      child: image == null? Text("No Image Selected"): Image.file(image),
+    ));
+
+    stackChildren.addAll(renderBoxes(size));
+
+    if (_busy){
+      stackChildren.add(const Center(child: CircularProgressIndicator(),
+      ));
+    }
     final user = FirebaseAuth.instance.currentUser!;
-    return Scaffold(
+    return  Scaffold(
       //appBar: AppBar(title: const Text(_title)),
-      body: Padding(
+      body:
+        Padding(
           padding: const EdgeInsets.all(32),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+            Stack(
+            children: stackChildren,
+            ),
               MaterialButton(
                   color: Colors.blue,
                   child: const Text(
@@ -85,7 +220,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                   ),
                   onPressed: () {
                     pickImageC();
-                  }
+                  },
               ),
               const SizedBox(height: 40),
               image != null? Image.file(image!,height: 250,width: 250,): const Text('No Image Selected'),
@@ -111,8 +246,9 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                 onPressed: () => FirebaseAuth.instance.signOut(),
               ),
             ],
-          )
+          ),
       ),
+
     );
   }
 
